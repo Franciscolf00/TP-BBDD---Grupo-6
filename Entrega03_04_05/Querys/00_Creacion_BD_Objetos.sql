b9ed858add178dc85fbe8edbfc08b833ece796ca
Franciscolf00 <franciscoleonelfranco@gmail.com>
2024-11-12 18:21:22 -0300

    Informe

---
+++ Entrega03_04_05/Querys/00_Creacion_BD_Objetos.sql
@@ -0,0 +1,170 @@
+-- Entrega 5/11/2024
+-- Grupo 06
+-- Materia: Base de Datos Aplicadas-2900
+-- Alumnos:
+-- Chaves, Juliana - 44005820
+-- Perez, Thiago Exequiel - 43097103
+-- Arce, Tomas Agustin - 43877113
+-- Franco, Francisco Leonel - 42116434
+
+-- Consigna:
+/*
+Luego de decidirse por un motor de base de datos relacional, llegó el momento de generar la base de datos. 
+Deberá instalar el DMBS y documentar el proceso. No incluya capturas de pantalla. 
+Detalle las configuraciones aplicadas (ubicación de archivos, memoria asignada, seguridad, puertos, etc.) 
+en un documento como el que le entregaría al DBA. Cree la base de datos, entidades y relaciones. 
+Incluya restricciones y claves. Deberá entregar un archivo .sql con el script completo de 
+creación (debe funcionar si se lo ejecuta “tal cual” es entregado). Incluya comentarios para indicar qué hace cada módulo de código. 
+Genere store procedures para manejar la inserción, modificado, borrado (si corresponde, 
+también debe decidir si determinadas entidades solo admitirán borrado lógico) de cada tabla. 
+Los nombres de los store procedures NO deben comenzar con “SP”.  
+Genere esquemas para organizar de forma lógica los componentes del sistema y aplique esto en la creación de objetos. NO use el esquema “dbo”.  
+*/
+
+CREATE DATABASE Com2900G06 COLLATE Modern_Spanish_CI_AS
+-------------------------------------------------------------------------------------
+--CONFIGURACIONES INICIALES
+--Para poder habilitar "Ole Automation Procedures"
+EXEC sp_configure 'show advanced options', 1;
+GO
+--Para poder acceder a fuentes de datos externas(archivo xslx de Excel) con OPENROWSET
+sp_configure 'Ad Hoc Distributed Queries', 1;
+GO
+--Para poder usar los procedures automaticos de OLE dentro de nuestros propios SP
+EXEC sp_configure 'Ole Automation Procedures', 1;
+RECONFIGURE;
+--EXEC sp_MSSet_oledb_prop N'Microsoft.ACE.OLEDB.12.0', N'AllowInProcess', 1;
+--EXEC sp_MSSet_oledb_prop N'Microsoft.ACE.OLEDB.12.0', N'DynamicParameters', 1;
+
+SET DATEFORMAT mdy; --Seteo el formato de fecha al formato que tienen los archivos
+-------------------------------------------------------------------------------------
+--use master;drop database Com2900G06;
+
+USE Com2900G06
+GO
+CREATE SCHEMA dbProducto
+GO
+CREATE SCHEMA dbVenta
+GO
+CREATE SCHEMA dbSucursal
+GO
+CREATE SCHEMA dbReporte
+GO
+CREATE SCHEMA dbFactura
+GO
+create or alter function dbVenta.RutaImportacion()
+returns VARCHAR(max)
+AS
+BEGIN
+	RETURN 'C:\Users\Tomas_Arce\Documents\GitHub\TP-BBDD---Grupo-6\TP_integrador_Archivos'; --Aca copiaríaas tu ruta base hasta los archivos.
+END
+go
+
+DROP TABLE IF EXISTS dbVenta.Venta;
+DROP TABLE IF EXISTS dbSucursal.Empleado;    
+DROP TABLE IF EXISTS dbSucursal.Sucursal; 
+DROP TABLE IF EXISTS dbProducto.Producto; 
+DROP TABLE IF EXISTS dbProducto.Categoria;   
+DROP TABLE IF EXISTS dbProducto.LineaDeProducto; 
+DROP TABLE IF EXISTS dbVenta.MetodoDePago;   
+DROP TABLE IF EXISTS dbFactura.Factura;
+DROP TABLE IF EXISTS dbFactura.DetalleDeFactura;
+DROP TABLE IF EXISTS dbFactura.NotaDeCredito;
+
+CREATE TABLE dbSucursal.Sucursal(
+	IDSucursal INT IDENTITY(1,1) PRIMARY KEY,
+	direccion VARCHAR(100),
+	numTelefono CHAR(9),
+	ciudad VARCHAR(50),
+	sucursal VARCHAR(50),
+	estado BIT,
+	fechaBaja DATETIME
+)
+go
+CREATE TABLE dbSucursal.Empleado(
+	Legajo INT PRIMARY KEY,
+	dni INT UNIQUE,
+	nombre VARCHAR(40),
+	apellido VARCHAR(20),
+	emailEmpresa VARCHAR(100) CHECK(emailEmpresa like '%@superA.com'),
+	emailPersonal VARCHAR(100) CHECK(emailPersonal like '%@%.com'),
+	direccion VARCHAR(100),
+	cargo CHAR(22) CHECK(cargo in ('Cajero', 'Supervisor', 'Gerente de sucursal')),
+	turno VARCHAR(16) CHECK(turno in('TM', 'TT' , 'Jornada Completa')),
+	FKSucursal INT NOT NULL REFERENCES dbSucursal.Sucursal(IDSucursal),
+	estado BIT,
+	fechaBaja DATETIME
+)
+go
+CREATE TABLE dbProducto.LineaDeProducto(
+	IDLineaDeProducto INT IDENTITY(1,1) PRIMARY KEY,
+	nombre VARCHAR(30),
+	estado BIT,
+	fechaBaja DATETIME
+)
+go
+CREATE TABLE dbProducto.Categoria(
+	IDCategoria INT IDENTITY(1,1) PRIMARY KEY,
+	nombre VARCHAR(50),
+	FKLineaDeProducto INT NOT NULL REFERENCES dbProducto.LineaDeProducto(IDLineaDeProducto),
+	estado BIT,
+	fechaBaja DATETIME
+)
+go
+CREATE TABLE dbProducto.Producto(
+	IDProducto INT IDENTITY(1,1) PRIMARY KEY,
+	nombre VARCHAR(100),
+	precioUnitario DECIMAL(10,2),
+	precioReferencia DECIMAL(10,2),
+	unidadReferencia VARCHAR(20),
+	fechaCreacion SMALLDATETIME,
+	FKCategoria INT NOT NULL REFERENCES dbProducto.Categoria(IDCategoria),
+	estado BIT,
+	fechaBaja DATETIME
+)
+go
+CREATE TABLE dbVenta.MetodoDePago(
+	IDMetodoDePago INT IDENTITY (1,1) PRIMARY KEY,
+	nombre VARCHAR(11),
+	estado BIT,
+	fechaBaja DATETIME
+)
+go
+CREATE TABLE dbFactura.Factura(
+	IDFactura INT IDENTITY (1,1) PRIMARY KEY,
+	numeroFactura INT,	--Lo tengo que guardar como int para verificar duplicados a la hora de insertar
+	tipoFactura CHAR(1) CHECK(tipoFactura in ('A', 'B', 'C')),
+	fechaHoraEmision DATETIME,
+	estadoFactura CHAR(1) CHECK(estadoFactura in ('E','P')),	--Emitida,Pagada
+	total real
+)
+go
+CREATE TABLE dbFactura.DetalleDeFactura(
+	IDDetalle INT IDENTITY (1,1) PRIMARY KEY,
+	cantidad INT,
+	subtotal real,
+	precioUnitarioAlMomento real,
+	FKProducto INT NOT NULL REFERENCES dbProducto.Producto(IDProducto),
+	FKFactura INT NOT NULL REFERENCES dbFactura.Factura(IDFactura)
+)
+go
+CREATE TABLE dbFactura.NotaDeCredito(
+	IDNotaDeCredito INT IDENTITY (1,1) PRIMARY KEY,
+	motivo VARCHAR(150),
+	fechaHoraNota DATETIME,
+	FKFactura INT NOT NULL REFERENCES dbFactura.Factura(IDFactura)
+)
+go
+CREATE TABLE dbVenta.Venta(
+	IDVenta INT IDENTITY(1,1) PRIMARY KEY,
+	tipoCliente CHAR(6) CHECK(tipoCliente in ('Member', 'Normal')),
+	genero CHAR(6) CHECK(genero in ('Male', 'Female')),
+	fechaHoraVenta DATETIME,
+	identificadorDePago VARCHAR(30) CHECK((LEN(identificadorDePago) = 22 AND identificadorDePago LIKE '[0-9][0-9][0-9][0-9][0-9][0-9][0-9][0-9][0-9][0-9][0-9][0-9][0-9][0-9][0-9][0-9][0-9][0-9][0-9][0-9][0-9][0-9]')
+											OR (LEN(identificadorDePago) = 19 AND identificadorDePago LIKE '[0-9][0-9][0-9][0-9]-[0-9][0-9][0-9][0-9]-[0-9][0-9][0-9][0-9]-[0-9][0-9][0-9][0-9]')								
+											OR identificadorDePago IS NULL),
+	FKEmpleado INT NOT NULL REFERENCES dbSucursal.Empleado(Legajo),
+	FKMetodoDePago INT NOT NULL REFERENCES dbVenta.MetodoDePago(IDMetodoDePago),
+	FKSucursal INT NOT NULL REFERENCES dbSucursal.Sucursal(IDSucursal),
+	FKFactura INT NOT NULL REFERENCES dbFactura.Factura(IDFactura)
+)
---
+++ Entrega03_04_05/Querys/02_LotesDePrueba.sql
@@ -0,0 +1,360 @@
+USE Com2900G06
+ --use master
+ /*///////////////////////////////////////////////////////////////////////////////////////// */
+ --Prueba INSERCIONES
+ --METODO DE PAGO:	3 rows affected
+ GO
+-- Caso 1: Nombre válido
+EXEC dbVenta.InsertarMetodoDePago @nombre = 'Cash'; --Debería insertarse correctamente.
+GO
+-- Caso 2: Nombre válido
+EXEC dbVenta.InsertarMetodoDePago 'Credit card'; --Debería insertarse correctamente.
+GO
+-- Caso 3: Nombre vacío
+EXEC dbVenta.InsertarMetodoDePago @nombre = ''; --'Falta nombre.'
+GO
+-- Caso 4: Nombre demasiado largo
+EXEC dbVenta.InsertarMetodoDePago @nombre = 'Tarjeta de Crédito'; --'Nombre demasiado largo. Tamaño maximo de 11 caracteres.'
+GO
+-- Caso 5: Nombre que ya existe
+EXEC dbVenta.InsertarMetodoDePago @nombre = 'Cash'; --'El nombre del metodo de pago ingresado ya existe.'
+GO
+-- Caso 6: Nombre demasiado largo
+EXEC dbVenta.InsertarMetodoDePago @nombre = 'Transferencia'; --'Nombre demasiado largo. Tamaño maximo de 11 caracteres.'
+GO
+-- Caso 7: Nombre válido
+EXEC dbVenta.InsertarMetodoDePago @nombre = 'Ewallet'; --Debería insertarse correctamente.
+GO
+----------------------------------------------------------------------------------------------
+--SUCURSAL: 2 rows affected
+-- Caso 1: Inserción válida
+EXEC dbSucursal.InsertarSucursal 
+    @direccion = 'Calle Falsa 123',
+    @numTelefono = '123456789',
+    @ciudad = 'Madrid',
+    @sucursal = 'Sucursal Centro'; -- Debería insertarse correctamente.
+GO
+-- Caso 2: Inserción válida
+EXEC dbSucursal.InsertarSucursal 
+    @direccion = 'Avenida Siempre Viva 742',
+    @numTelefono = '987654321',
+    @ciudad = 'Barcelona',
+    @sucursal = 'Sucursal Norte'; -- Debería insertarse correctamente.
+GO
+-- Caso 3: Inserción con varios errores
+EXEC dbSucursal.InsertarSucursal 
+    @direccion = '',				--Falta direccion
+    @numTelefono = '12345',     
+    @ciudad = 'MuyLargaCiudad',      -- Ciudad demasiado larga (más de 9 caracteres).
+    @sucursal = 'Sucursal Centro';     --Repetida.
+GO
+----------------------------------------------------------------------------------------------
+--EMPLEADO: 2 rows affected
+EXEC dbVenta.InsertarEmpleado
+	@Legajo = NULL,          -- Legajo vacío o nulo
+	@dni = NULL,             -- DNI vacío o nulo
+	@nombre = '',            -- Nombre vacío
+	@apellido = 'EsteApellidoEsDemasiadoLargoParaElCampo',  -- Apellido demasiado largo
+	@emailEmpresa = 'empleado@otroDominio.com',             -- Email de empresa no válido
+	@emailPersonal = 'personal@correo',                     -- Email personal sin ".com"
+	@direccion = NULL,                                       -- Dirección vacía
+	@cargo = 'Mantenimiento',                                -- Cargo no válido
+	@turno = 'Noche',                                        -- Turno no válido
+	@FKSucursal = 99999;                                     -- ID de sucursal que no existe
+GO
+EXEC dbVenta.InsertarEmpleado
+    @Legajo = 12345,
+    @dni = 45678901,
+    @nombre = 'Carlos',
+    @apellido = 'Gomez',
+    @emailEmpresa = 'carlos.gomez@superA.com',
+    @emailPersonal = 'carlos.gomez@gmail.com',
+    @direccion = 'Calle Falsa 123',
+    @cargo = 'Cajero',
+    @turno = 'TM',
+    @FKSucursal = 1; 
+GO
+EXEC dbVenta.InsertarEmpleado
+    @Legajo = 54321,
+    @dni = 98765432,
+    @nombre = 'Lucia',
+    @apellido = 'Perez',
+    @emailEmpresa = 'lucia.perez@superA.com',
+    @emailPersonal = 'lucia.perez@yahoo.com',
+    @direccion = 'Avenida Principal 456',
+    @cargo = 'Supervisor',
+    @turno = 'Jornada Completa',
+    @FKSucursal = 2; 
+GO
+EXEC dbVenta.InsertarEmpleado 
+    @Legajo = 12345,
+    @dni = 66778899,
+    @nombre = 'Pedro',
+    @apellido = 'Sánchez',
+    @emailEmpresa = 'pedro.sanchez@superA.com',
+    @emailPersonal = 'pedro.sanchez@gmail.com',
+    @direccion = 'Calle Real 654',
+    @cargo = 'Gerente de sucursal',
+    @turno = 'Jornada Completa',
+    @FKSucursal = 999; -- ID de sucursal no existente. Debería generar un error: 'El ID de sucursal ingresado no existe.'
+GO
+----------------------------------------------------------------------------------------------
+--LINEA DE PRODUCTO: 2 rows affected
+EXEC dbProducto.InsertarLineaDeProducto
+    @nombreLineaDeProducto = 'Lacteos';
+GO
+EXEC dbProducto.InsertarLineaDeProducto
+    @nombreLineaDeProducto = 'Bebidas';
+GO
+EXEC dbProducto.InsertarLineaDeProducto
+    @nombreLineaDeProducto = 'Bebidas';
+GO
+EXEC dbProducto.InsertarLineaDeProducto
+    @nombreLineaDeProducto = '';  
+GO
+EXEC dbProducto.InsertarLineaDeProducto
+    @nombreLineaDeProducto = 'EstaEsUnaLineaDeProductoQueSuperaElLimiteDe30Caracteres';
+GO
+----------------------------------------------------------------------------------------------
+--CATEGORIA: 2 rows affected
+EXEC dbProducto.InsertarCategoria
+    @nombreCategoria = '',            -- Nombre de categoría vacío
+    @FKLineaDeProducto = NULL;        -- ID de línea de producto nulo
+GO
+EXEC dbProducto.InsertarCategoria
+    @nombreCategoria = 'EstaCategoriaTieneUnNombreExcesivamenteLargoQueSuperaLos50Caracteres',
+    @FKLineaDeProducto = 9999;  -- ID de línea de producto que no existe
+GO
+EXEC dbProducto.InsertarCategoria
+    @nombreCategoria = 'Quesos',
+    @FKLineaDeProducto = 1; 
+GO
+EXEC dbProducto.InsertarCategoria
+    @nombreCategoria = 'Jugos',
+    @FKLineaDeProducto = 2; 
+GO
+EXEC dbProducto.InsertarCategoria
+    @nombreCategoria = 'Jugos',
+    @FKLineaDeProducto = 2;  
+GO
+----------------------------------------------------------------------------------------------
+--PRODUCTO: 2 rows affected
+EXEC dbProducto.InsertarProducto
+    @nombre = '',                 -- Nombre vacío
+    @precioUnitario = 0,          -- Precio unitario menor o igual a 0
+    @precioReferencia = -5,       -- Precio de referencia menor o igual a 0
+    @unidadReferencia = '',       -- Unidad de referencia vacía
+    @FKCategoria = NULL;          -- FK de categoría nulo
+GO
+EXEC dbProducto.InsertarProducto
+    @nombre = 'ProductoConUnNombreExcesivamenteLargoQueSuperaLos50Caracteres',
+    @precioUnitario = 12,
+    @precioReferencia = NULL,
+    @unidadReferencia = 'UnidadDeReferenciaDemasiadoLarga',
+    @FKCategoria = 9999; -- ID de categoría que no existe
+GO
+EXEC dbProducto.InsertarProducto
+    @nombre = 'Leche LaSerenisima',
+    @precioUnitario = 20,
+    @precioReferencia = 18,
+    @unidadReferencia = 'Litro',
+    @FKCategoria = 1;
+GO
+EXEC dbProducto.InsertarProducto
+    @nombre = 'Leche LaSerenisima',  -- Nombre de producto ya existente
+    @precioUnitario = 20,
+    @precioReferencia = 18,
+    @unidadReferencia = 'Litro',
+    @FKCategoria = 1;
+GO
+EXEC dbProducto.InsertarProducto
+    @nombre = 'Producto 2',
+    @precioUnitario = 30,
+    @precioReferencia = 40,
+    @unidadReferencia = 'unidad pro',
+    @FKCategoria = 1;
+GO
+----------------------------------------------------------------------------------------------
+--FACTURAS, DETALLES Y VENTA(3 rows affected)
+CREATE OR ALTER PROCEDURE dbFactura.PruebaFacturasYVentas
+AS
+BEGIN
+	DECLARE @IDFactura INT;
+	--Creo la factura
+	print 'creo factura'
+	EXEC dbFactura.CrearFactura @IDFacturaGenerada=@IDFactura OUTPUT;
+
+	--Inserto detalles
+	EXEC dbFactura.InsertarDetalleDeFactura
+		@cantidad=-10,				--Cantidad negativa
+		@FKProducto=1000000,		--Producto no existente
+		@FKFactura=1000;			--Factura no existente
+	print 'inserto 2 detalles'
+	EXEC dbFactura.InsertarDetalleDeFactura
+		@cantidad=5,
+		@FKProducto=1,
+		@FKFactura=@IDFactura;
+	EXEC dbFactura.InsertarDetalleDeFactura
+		@cantidad=2,
+		@FKProducto=2,
+		@FKFactura=@IDFactura;
+
+	--Emito la factura
+	EXEC dbFactura.EmitirFactura
+		@IDFactura=7645,			--Factura no existente
+		@numeroFactura=0,			--Invalido
+		@tipoFactura='Z';			--Tipo Factura inválido
+	
+	print 'emito la factura'
+	EXEC dbFactura.EmitirFactura
+		@IDFactura=@IDFactura,			
+		@numeroFactura=546665237,	
+		@tipoFactura='A';
+
+	--Le asignó la factura a la venta
+	EXEC dbVenta.InsertarVenta
+		@tipoCliente = 'VIP',					-- Tipo de cliente inválido
+		@genero = 'Z',							-- Género inválido
+		@identificadorDePago = '12345678abcd',  -- Identificador de pago incorrecto
+		@FKempleado = 9999,						-- Legajo no registrado
+		@FKMetodoDePago = 9999,					-- ID de método de pago no existente
+		@FKSucursal = 9999,						-- ID de sucursal no existente
+		@FKFactura = 9999;						-- ID de factura no existente
+
+	EXEC dbVenta.InsertarVenta
+		@tipoCliente = 'Normal',       -- Tipo de cliente válido
+		@genero = 'M',                 -- Género inválido
+		@identificadorDePago = '1234-5678-90aa-5678', -- Identificador de pago mal formateado
+		@FKempleado = NULL,            -- Legajo vacío
+		@FKMetodoDePago = 0,           -- ID de método de pago nulo
+		@FKSucursal = 0,               -- ID de sucursal nulo
+		@FKFactura = 9999;			   -- ID de factura nulo
+	print 'le asigno venta a factura'
+	EXEC dbVenta.InsertarVenta
+		@tipoCliente = 'Normal',
+		@genero = 'Male',
+		@identificadorDePago = '1234-5678-9012-3456', 
+		@FKempleado = 54321,              
+		@FKMetodoDePago = 1,                       
+		@FKSucursal = 1,
+		@FKFactura = @IDFactura;
+
+	EXEC dbVenta.InsertarVenta
+		@tipoCliente = 'Member',
+		@genero = 'Female',
+		@identificadorDePago = NULL,   
+		@FKempleado = 54321,               
+		@FKMetodoDePago = 2,                         
+		@FKSucursal = 2,
+		@FKFactura = @IDFactura;			--La factura ya tiene una venta asociada
+	-----
+	--Pruebo que no deje emitir si no agregue ningun detalle
+	print 'creo la factura'
+	EXEC dbFactura.CrearFactura @IDFacturaGenerada=@IDFactura OUTPUT;
+	print 'trato de emitir, falla'
+	EXEC dbFactura.EmitirFactura		--Falta al menos un detalle
+		@IDFactura=@IDFactura,			
+		@numeroFactura=867662468,	
+		@tipoFactura='B';
+	print 'trato de asociar, falla'
+	EXEC dbVenta.InsertarVenta			--Falta que se emita la factura
+		@tipoCliente = 'Member',
+		@genero = 'Female',
+		@identificadorDePago = NULL,   
+		@FKempleado = 54321,               
+		@FKMetodoDePago = 2,                         
+		@FKSucursal = 2,
+		@FKFactura = @IDFactura;
+END
+GO
+EXEC dbFactura.PruebaFacturasYVentas;
+----------------------------------------------------------------------------------------------
+/*///////////////////////////////////////////////////////////////////////////////////////// */
+/*///////////////////////////////////////////////////////////////////////////////////////// */
+/*///////////////////////////////////////////////////////////////////////////////////////// */
+--Prueba ACTUALIZACIONES
+GO
+EXEC dbSucursal.ActualizarSucursal
+    @IDSucursal = 1,  -- ID de la sucursal a actualizar
+    @direccion = 'Calle Actualizada 123',  -- Nueva dirección
+    @numTelefono = '987654321',  -- Nuevo número de teléfono
+    @ciudad = 'Ciudad Actualizada',  -- Nueva ciudad
+    @sucursal = 'Sucursal Actu';  -- Nuevo nombre de la sucursal
+GO
+EXEC dbSucursal.ActualizarEmpleado
+	@empleadoAactualizar=54321,
+	@dni=1111111,
+	@nombre='Empleado Actu',
+	@apellido='Alizado',
+	@emailEmpresa='emailempresaactu@superA.com',
+	@emailPersonal='emailpersonalactu@unlam.com',
+	@direccion='direccion actualizada 5421',
+	@cargo='Supervisor',
+	@turno='TM',
+	@FKSucursal=100;
+GO
+EXEC dbProducto.ActualizarLineaDeProducto
+	@lineaDeProductoAactualizar=2,
+	@nombre='linea de producto actualizada';
+GO
+EXEC dbProducto.ActualizarCategoria
+	@categoriaAactualizar=1,
+	@nombre='categoria actualizada',
+	@FKLineaDeProducto=2;
+GO
+EXEC dbProducto.ActualizarProducto
+	@productoAactualizar=5,
+	@nombre='producto actualizado',
+	@precioUnitario=48.6,
+	@precioReferencia=50.0,
+	@unidadReferencia='1 unidad actu',
+	@FKCategoria=2;
+GO
+EXEC dbVenta.ActualizarMetodoDePago
+	 @metodoDePagoAactualizar=1,
+	 @nombre='Chachos';
+GO
+/*///////////////////////////////////////////////////////////////////////////////////////// */
+/*///////////////////////////////////////////////////////////////////////////////////////// */
+/*///////////////////////////////////////////////////////////////////////////////////////// */
+--Prueba BORRADOS(LÓGICOS)
+EXEC dbSucursal.ModificarEstadoSucursal
+	@IDSucursal=2,
+	@estado=1;
+GO
+EXEC dbSucursal.ModificarEstadoEmpleado
+	@Legajo=12345,
+	@estado=0;
+GO
+EXEC dbProducto.ModificarEstadoLineaDeProducto
+	@IDLineaDeProducto=3,
+	@estado=1;
+GO
+EXEC dbProducto.ModificarEstadoCategoria
+	@IDCategoria=1,
+	@estado=0;
+GO
+EXEC dbProducto.ModificarEstadoProducto
+	@IDProducto=2,
+	@estado=0;
+GO
+EXEC dbVenta.ModificarEstadoMetodoDePago
+	@IDMetodoDePago=1,
+	@estado=1;
+GO
+
+
+SELECT * FROM dbSucursal.Sucursal
+GO
+SELECT * FROM dbSucursal.Empleado
+GO
+SELECT * FROM dbProducto.LineaDeProducto
+GO
+SELECT * FROM dbProducto.Categoria
+GO
+SELECT * FROM dbProducto.Producto
+GO
+SELECT * FROM dbVenta.MetodoDePago
+GO
+SELECT * FROM dbVenta.Venta
+
